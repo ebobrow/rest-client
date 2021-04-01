9e5e4be91103302953c6959009237126d778c16d
Elliot Bobrow <elliotgreybobrow@gmail.com>
2021-04-01 09:49:43 -0700

    change syntax and add headers

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # REST Client
+
+```
+# GET slash route
+http://localhost:3000
+GET /
+
+# POST to /api/auth/register
+http://localhost:3000
+
+# Headers
+Content-Type: application/json
+Authorization: bearer alghdlaiusdflhsadfkjsadf
+
+# Body
+{
+	"username": "name",
+	"password": "password123"
+}
+
+POST /auth/register
+```
--- src/curl.rs
+++ src/curl.rs
@@ -3,40 +3,89 @@ use std::process::Command;
 use std::{fs, io};
 
 struct Request {
+    text: String,
     method: String,
-    url: String,
-    body: Option<String>,
+    // TODO: This is not accurate b/c comments and blank lines are filtered out
+    first_line: u32,
+    last_line: u32,
 }
 
+const METHODS: [&str; 7] = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE", "PATCH"];
+
 impl Request {
-    fn new(method: String, url: &str, body: Option<String>) -> Request {
+    fn new(text: &str, method: &str, first_line: u32, last_line: u32) -> Request {
         Request {
-            method,
-            url: url.to_string(),
-            body,
+            text: text.to_string(),
+            method: method.to_string(),
+            first_line,
+            last_line,
         }
     }
 
-    fn send(self) {
-        let body_content: String; // TODO: Is this the best way to do this?
-        let args = match self.body {
-            Some(body) => {
-                body_content = body;
-                vec![
-                    "-X",
-                    &self.method,
-                    "-H",
-                    "Content-Type: application/json",
-                    "-d",
-                    &body_content,
-                    &self.url,
-                    "-i",
-                ]
-            }
+    fn error(&self, line: u32, msg: &str) {
+        println!("Error (line {}): {}", line, msg);
+    }
+
+    fn parse(self) {
+        let mut lines = self.text.lines();
+        let base_url = lines.next();
+        let mut base_url = match base_url {
+            Some(u) => u.to_string(),
             None => {
-                vec!["-X", &self.method, &self.url, "-i"]
+                self.error(self.first_line, "Expected URL");
+                return;
             }
         };
+
+        let mut cur_line = self.first_line;
+
+        let mut in_body = false;
+        let mut body = "".to_string();
+
+        let mut headers: Vec<&str> = vec![];
+
+        for line in lines {
+            cur_line += 1;
+            if cur_line == self.last_line {
+                let mut words = line.split(' ');
+                words.next();
+                let url = match words.next() {
+                    Some(u) => u,
+                    None => {
+                        self.error(self.last_line, "Expected URL");
+                        ""
+                    }
+                };
+                base_url.push_str(url);
+            } else if in_body || line.starts_with('{') {
+                body.push_str(line);
+                // TODO: Nested json, like:
+                // {
+                // "value": {
+                // "inner_value": "sfgsdfg"
+                // } <- Would trigger end
+                // }
+                in_body = !line.ends_with('}');
+            } else {
+                // Assume headers
+                headers.push(line);
+            }
+        }
+
+        let mut args: Vec<&str> = vec!["-X", &self.method];
+        for header in headers {
+            args.push("-H");
+            args.push(header);
+        }
+        if body != "" {
+            args.push("-d");
+            args.push(&body);
+        }
+        args.push(&base_url);
+        args.push("-i");
+
+        println!("{} {}\n", self.method, base_url);
+
         let output = Command::new("curl")
             .args(&args)
             .output()
@@ -48,43 +97,27 @@ impl Request {
 pub fn parse_input(filename: &str) {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let requests = contents.split("\n\n");
-    for request in requests {
-        handle_request(request);
-    }
-}
+    let mut cur_idx = 0;
+    let mut start_idx = 0;
+    let mut cur_line = 0;
+    let mut start_line = 1;
 
-fn handle_request(request: &str) {
-    let mut lines = request
+    let filtered: String = contents
         .lines()
-        .filter(|line| !line.is_empty() && !line.starts_with('#'));
+        .filter(|line| !line.is_empty() && !line.starts_with('#'))
+        .map(|line| format!("{}\n", line)) // TODO: this feels hacky
+        .collect();
 
-    if let Some(line) = lines.next() {
-        println!("\n---------------\n");
-        println!("{}", line);
-        let mut words = line.split(' ');
-
-        let method = words.next().expect("Invalid syntax: Method required");
-        let url = words.next().expect("Invalid syntax: URL required");
-
-        let body: String = lines.collect();
-        let body = if body.is_empty() {
-            println!();
-            None
-        } else {
-            println!("Body: {}\n", body.trim());
-            Some(body)
-        };
-
-        match method {
-            "GET" | "HEAD" | "OPTIONS" => {
-                Request::new(method.to_string(), url, None).send();
+    for line in filtered.lines() {
+        cur_idx += line.len() + 1;
+        cur_line += 1;
+        for method in METHODS.iter() {
+            if line.starts_with(method) {
+                println!("\n---------------\n");
+                Request::new(&filtered[start_idx..cur_idx], method, start_line, cur_line).parse();
+                start_idx = cur_idx;
+                start_line = cur_line + 1;
             }
-            "POST" | "PUT" | "DELETE" | "PATCH" => {
-                Request::new(method.to_string(), url, body).send();
-            }
-            _ => println!("Invalid method: {}", method),
-        };
-        println!();
+        }
     }
 }
